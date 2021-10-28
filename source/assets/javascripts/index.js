769151dc1166162ab97d01d2b4838b1fb33a4668
Troy Russell <troyjruss@gmail.com>
2021-10-28 23:06:21 +0900

    js attempt

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,1 +1,15 @@
 import 'bootstrap';
+
+const navToggle = nav.querySelector('#nav-toggle');
+let isNavExpanded = navToggle.getAttribute('aria-expanded') === 'true';
+
+const toggleNavVisibility = () => {
+    isNavExpanded == !isNavExpanded;
+    navToggle.setAttribute('aria-expanded'), isNavExpanded);
+};
+
+navToggle.addEventListener('click', toggleNavVisibility);
+
+const goo = document.querySelector('.nav-container');
+
+goo.addEventListener('click', () => goo.classList.add("orange"));
