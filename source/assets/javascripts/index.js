cd4cfd6bb37139332ecec57aa9ebf4cae31ba444
Troy Russell <troyjruss@gmail.com>
2021-10-30 22:42:21 +0900

    mobile menu

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -13,3 +13,9 @@ navToggle.addEventListener('click', toggleNavVisibility);
 // const goo = document.querySelector('.nav-container');
 
 // goo.addEventListener('click', () => goo.classList.add("orange"));
+
+const navMenu = document.querySelector('#nav-menu');
+const navLinksContainer = navMenu.querySelector('.nav-links');
+
+navLinksContainer.addEventListener('click', (e) => e.stopPropagation());
+navMenu.addEventListener('click', toggleNavVisibility);
