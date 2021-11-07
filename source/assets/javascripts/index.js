aa4aa1d4e6699653b173d0693e539594b76d9135
Troy Russell <troyjruss@gmail.com>
2021-11-07 23:55:15 +0900

    js toggle start

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap';
 
+// nav toggle
 const navToggle = nav.querySelector('#nav-toggle');
 let isNavExpanded = navToggle.getAttribute('aria-expanded') === 'true';
 
@@ -19,3 +20,35 @@ const navLinksContainer = navMenu.querySelector('.nav-links');
 
 navLinksContainer.addEventListener('click', (e) => e.stopPropagation());
 navMenu.addEventListener('click', toggleNavVisibility);
+
+// dark mode toggle
+const details = document.getElementById('details');
+const basics = document.getElementById('basics');
+const basicInfo = document.querySelector('.basic-info-group');
+const detailedInfo = document.querySelector('.detailed-info-group');
+const backingCard = document.querySelector('.backing-card');
+
+if (details) {
+    details.addEventListener('click', () => {
+    console.log("details!");
+    basicInfo.classList.add('basics-hidden');
+    detailedInfo.classList.remove('details-hidden');
+    detailedInfo.classList.add('backing-card-details');
+    backingCard.classList.add('backing-card-details');
+    });
+}
+
+if (basics) {
+    basics.addEventListener('click', () => {
+    console.log("basics!");
+    detailedInfo.classList.add('details-hidden');
+    basicInfo.classList.remove('basics-hidden');
+    backingCard.classList.remove('backing-card-details');
+    });
+}
+  
+    // basicInfo.addEventListener('click', () => {
+    //   console.log("info!");
+    // });
+
+  
--- source/assets/javascripts/nav.js
+++
@@ -1,9 +0,0 @@
-const navToggle = nav.querySelector('#nav-toggle');
-let isNavExpanded = navToggle.getAttribute('aria-expanded') === 'true';
-
-const toggleNavVisibility = () => {
-    isNavExpanded = !isNavExpanded;
-    navToggle.setAttribute('aria-expanded'), isNavExpanded);
-};
-
-navToggle.addEventListener('click', toggleNavVisibility);
