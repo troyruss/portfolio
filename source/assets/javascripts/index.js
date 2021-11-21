33cef133030482ecffdf9e0c83e5b28a4eab5f5f
Troy Russell <troyjruss@gmail.com>
2021-11-21 23:14:06 +0900

    working on darkmode toggle js

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -22,39 +22,56 @@ navLinksContainer.addEventListener('click', (e) => e.stopPropagation());
 navMenu.addEventListener('click', toggleNavVisibility);
 
 // dark mode toggle need to update
-const details = document.getElementById('details');
-const basics = document.getElementById('basics');
-const basicInfo = document.querySelector('.basic-info-group');
-const detailedInfo = document.querySelector('.detailed-info-group');
-const backingCard = document.querySelector('.backing-card');
+// const details = document.getElementById('details');
+// const basics = document.getElementById('basics');
+// const basicInfo = document.querySelector('.basic-info-group');
+// const detailedInfo = document.querySelector('.detailed-info-group');
+// const backingCard = document.querySelector('.backing-card');
 
-if (details) {
-    details.addEventListener('click', () => {
-    console.log("details!");
-    // basicInfo.classList.add('basics-hidden');
-    // detailedInfo.classList.remove('details-hidden');
-    // detailedInfo.classList.add('backing-card-details');
-    // backingCard.classList.add('backing-card-details');
-    });
-}
+// if (details) {
+//     details.addEventListener('click', () => {
+//     console.log("details!");
+//     // basicInfo.classList.add('basics-hidden');
+//     // detailedInfo.classList.remove('details-hidden');
+//     // detailedInfo.classList.add('backing-card-details');
+//     // backingCard.classList.add('backing-card-details');
+//     });
+// }
 
-if (basics) {
-    basics.addEventListener('click', () => {
-    console.log("basics!");
-    // detailedInfo.classList.add('details-hidden');
-    // basicInfo.classList.remove('basics-hidden');
-    // backingCard.classList.remove('backing-card-details');
-    });
-}
+// if (basics) {
+//     basics.addEventListener('click', () => {
+//     console.log("basics!");
+//     // detailedInfo.classList.add('details-hidden');
+//     // basicInfo.classList.remove('basics-hidden');
+//     // backingCard.classList.remove('backing-card-details');
+//     });
+// }
   
-    // basicInfo.addEventListener('click', () => {
-    //   console.log("info!");
-    // });
+//     // basicInfo.addEventListener('click', () => {
+//     //   console.log("info!");
+//     // });
 
 // NEW DARK MODE
-let darkMode = localStorage.getItem('darkMode');
+const darkMode = localStorage.getItem('darkMode');
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 
+const enableDarkMode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkMode', 'enabled');
+};
+
+const disableDarkMode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkMode', 'disabled');
+};
+
 darkModeToggle.addEventListener('click', () => {
-    console.log('woo!');
+    console.log("ok");
+    if (darkMode === null || 'disabled') {
+        enableDarkMode();
+        console.log("enabledarkmode?");
+    } else if (darkMode === 'enabled') {
+        disableDarkMode();
+        console.log("disabledarkmode?")
+    }
 });
